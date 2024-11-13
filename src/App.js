815f07ff79fce7d685042e4d064690cd3e54f7d2
Y23MSWD31545 <1428raviroyal@gmail.com>
2024-11-13 14:38:16 +0530

    Update App.js and add NavBar.css for navigation styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+// frontend/src/App.js
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import StudentList from './components/StudentList';
+import BulkUpload from './components/BulkUpload';
+import './NavBar.css';
 
 function App() {
   return (
-    <div>
-      <h1>Student Management System</h1>
-      <StudentList />
-    </div>
+    <Router>
+      <nav>
+        <Link to="/students">Student</Link>
+        <Link to="/bulk-upload">Bulk Upload</Link>
+      </nav>
+      <Routes>
+        <Route path="/students" element={<StudentList />} />
+        <Route path="/bulk-upload" element={<BulkUpload />} />
+      </Routes>
+    </Router>
   );
 }
 
